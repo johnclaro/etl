263423f435751c4df184d7e083fcfadd3cd06dee
John Claro <jkrclaro@gmail.com>
2021-02-28 21:43:00 +0000

    Add transform and combine methods

--- covid.py
+++ covid.py
@@ -2,14 +2,35 @@ import pandas as pd
 
 
 def extract(url):
-    df = pd.read_csv(url, index_col=0)
+    df = pd.read_csv(url)
     return df
 
 
 def transform(df):
-    df.index = pd.to_datetime(df.index)
     df.columns = df.columns.str.strip().str.lower()
-    df.columns = df.columns.str.replace('/', '_')
+    df['date'] = pd.to_datetime(df['date'])
+    columns = {
+        '/': '_',
+        'confirmed': 'cases',
+        'country_region': 'country',
+        'province_state': 'state'
+    }
+    for bad_column, good_column in columns.items():
+        df.columns = df.columns.str.replace(bad_column, good_column)
+
+    try:
+        df = df[df.country == 'US']
+    except AttributeError:
+        pass
+
+    return df
+
+
+def combine(ny, jh):
+    df = ny.merge(jh, on='date', suffixes=('_ny', '_jh'))
+    df = df[['date', 'cases_ny', 'deaths_ny', 'recovered']]
+    df.columns = df.columns.str.replace('cases_ny', 'cases')
+    df.columns = df.columns.str.replace('deaths_ny', 'deaths')
     return df
 
 
@@ -17,8 +38,10 @@ def main():
     ny = extract('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv')
     jh = extract('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
 
+    ny = transform(ny)
     jh = transform(jh)
-    print(jh)
+
+    df = combine(ny, jh)
 
 
 if __name__ == '__main__':
