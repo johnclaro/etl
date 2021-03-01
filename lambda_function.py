2e10ef461267ef17f7350d726cb93fb1cd722bb0
John Claro <jkrclaro@gmail.com>
2021-03-01 22:15:12 +0000

    Hello from Github Action

--- lambda_function.py
+++ lambda_function.py
@@ -2,14 +2,14 @@ import covid
 
 
 def lambda_handler():
-    nyt = covid.etl.extract(covid.datasets.NEW_YORK_TIMES)
-    jh = covid.etl.extract(covid.datasets.JOHN_HOPKINS)
+    # nyt = covid.etl.extract(covid.datasets.NEW_YORK_TIMES)
+    # jh = covid.etl.extract(covid.datasets.JOHN_HOPKINS)
 
-    nyt = covid.etl.transform(nyt)
-    jh = covid.etl.transform(jh)
-    df = covid.etl.join(nyt, jh)
+    # nyt = covid.etl.transform(nyt)
+    # jh = covid.etl.transform(jh)
+    # df = covid.etl.join(nyt, jh)
 
-    print(df)
+    return {'status': 'Hello from Github Action'}
 
 
 if __name__ == '__main__':
