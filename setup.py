083fcdabce05d7a5bccca74141b1508e01dbea66
John Claro <jkrclaro@gmail.com>
2021-03-01 13:24:36 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open('requirements.txt') as reqs:
+        return reqs.read().splitlines()
+
+
+setup(
+    name='covid',
+    version='1.0',
+    packages=find_packages(exclude=['tests', 'tests.py']),
+    install_requires=get_requirements(),
+    author='John Claro',
+    author_email='jkrclaro@gmail.com',
+    description='ETL for Covid datasets',
+    keywords='etl covid',
+    url='https://github.com/johnclaro/covid.git'
+)
+
+
