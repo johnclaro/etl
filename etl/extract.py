3ccdb9f7cca18a8a08d7986c188a03ddb5244e1f
John Claro <jkrclaro@gmail.com>
2021-03-01 14:43:11 +0000

    Update namings

--- etl/extract.py
+++ etl/extract.py
@@ -1,15 +1,6 @@
 import pandas as pd
 
 
-def new_york_times():
-    url = 'https://raw.githubusercontent.com/nytimes/' \
-          'covid-19-data/master/us.csv'
-    df = pd.read_csv(url)
-    return df
-
-
-def john_hopkins():
-    url = 'https://raw.githubusercontent.com/datasets/covid-19/master/' \
-          'data/time-series-19-covid-combined.csv'
+def extract(url):
     df = pd.read_csv(url)
     return df
