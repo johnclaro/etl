da7d2a344a7277a8168da5b1a38ad628b4afb8b8
John Claro <jkrclaro@gmail.com>
2021-03-01 13:32:59 +0000

    Update datasets urls

--- datasets.py
+++ datasets.py
@@ -2,7 +2,5 @@ from enum import Enum
 
 
 class Dataset(Enum):
-    NEW_YORK_TIMES = 'https://raw.githubusercontent.com/nytimes/' \
-                     'covid-19-data/master/us.csv'
-    JOHN_HOPKINS = 'https://raw.githubusercontent.com/datasets/' \
-                   'covid-19/master/data/time-series-19-covid-combined.csv'
+    NEW_YORK_TIMES = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+    JOHN_HOPKINS = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
