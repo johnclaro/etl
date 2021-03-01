41eb5f80e92cc285f2fa42229afb466165a97223
John Claro <jkrclaro@gmail.com>
2021-03-01 14:43:29 +0000

    Add datasets

---
+++ datasets.py
@@ -0,0 +1,2 @@
+NEW_YORK_TIMES = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+JOHN_HOPKINS = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
