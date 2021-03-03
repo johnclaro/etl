e7618c5aaf0adccd46f1d2cdf052c3ed55268f68
John Claro <jkrclaro@gmail.com>
2021-03-03 16:22:43 +0000

    Change recovered to recoveries

--- covid/etl/transformation.py
+++ covid/etl/transformation.py
@@ -11,7 +11,8 @@ def clean(df):
         '/': '_',
         'confirmed': 'cases',
         'country_region': 'country',
-        'province_state': 'state'
+        'province_state': 'state',
+        'recovered': 'recoveries'
     }
     for bad_column, good_column in columns.items():
         df.columns = df.columns.str.replace(bad_column, good_column)
@@ -41,7 +42,7 @@ def filtrate(df, timeset):
 
 def join(nyt, jh):
     df = nyt.merge(jh, on='date', suffixes=('_nyt', '_jh'))
-    df = df[['date', 'cases_nyt', 'deaths_nyt', 'recovered']]
+    df = df[['date', 'cases_nyt', 'deaths_nyt', 'recoveries']]
     df.columns = df.columns.str.replace('cases_nyt', 'cases')
     df.columns = df.columns.str.replace('deaths_nyt', 'deaths')
     return df
