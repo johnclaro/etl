db02fe06775c826dd6f5c2462e76aeec6ba98250
John Claro <jkrclaro@gmail.com>
2021-03-05 22:14:34 +0000

    Remove layers

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open('requirements.txt') as reqs:
+        return reqs.read().splitlines()
+
+
+setup(
+    name='etl',
+    version='1.0',
+    author='John Claro',
+    author_email='jkrclaro@gmail.com',
+    description='ETL for Covid data',
+    keywords='etl covid',
+    url='https://github.com/johnclaro/etl.git',
+    packages=find_packages(include=['etl', 'etl.*']),
+    install_requires=get_requirements(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'etl=etl.cli:parse'
+        ]
+    }
+)
