934e04a7aabd218272bd64e9d163d7c4fe870071
John Claro <jkrclaro@gmail.com>
2021-03-06 17:31:17 +0000

    Switch URLs

--- etl/settings.py
+++ etl/settings.py
@@ -5,6 +5,6 @@ DAYS = 1
 PROD = bool(strtobool(os.environ.get('PROD', 'False')))
 
 if PROD:
-    URL = 'http://localhost:8000'
-else:
     URL = 'https://www.johnclaro.com'
+else:
+    URL = 'http://localhost:8000'
