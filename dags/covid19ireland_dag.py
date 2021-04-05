2ac276b95e2d7be300b3c7516067bbad8f6526eb
John Claro <jkrclaro@gmail.com>
2021-04-05 02:25:37 +0100

    Create dags folder for airflow

---
+++ dags/covid19ireland_dag.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0, 0, 0, 0),
+    'email': ['jkrclaro@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'covid19ireland_dag',
+    default_args=default_args,
+    description="Ireland's COVID-19 Data Hub",
+    schedule_interval=timedelta(minutes=1),
+)
+
+
+def cases():
+    print('Performing ETL on Cases')
+
+
+run_etl = PythonOperator(
+    task_id='cases',
+    python_callable=cases,
+    dag=dag,
+)
+
+run_etl
